bec37d7a692f5197bb8dd92895cbd55fbc05b363
KC <konrad.czechowski@gmail.com>
2019-02-05 17:36:36 +0100

    Change file structure, to not create virtual env inside mrunner directory.

--- setup_mrunner_venv_and_run.sh
+++ setup_mrunner_venv_and_run.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 # run with
 # bash mrunner_config.sh prometheus_username your_initials
+set -e
 export USERNAME=$1
 export INITIALS=$2
 
-git clone https://github.com/konradczechowski/mrunner_example
+# git clone https://github.com/konradczechowski/mrunner_example
 
 python3 -m venv mrunner_npt2
 source mrunner_npt2/bin/activate
 # install neptune
 pip install -I neptune-cli==2.8.23
-# Run experiment (with neptune) locally
-neptune run some_src/experiment.py
+
+# You should be able to run experiment (with neptune) locally, with
+# cd run 
+# neptune run some_src/experiment.py
 
 # Install mrunner
 pip install git+git://github.com/deepsense-ai/mrunner.git@bd1849cf
@@ -21,9 +24,9 @@ sed -r "s/^(\s*)(slurm_url: <username>@pro.cyfronet.pl$)/\1slurm_url: $USERNAME@
 sed -r "s/^(\s*)(user_id: <initials>$)/\1user_id: $INITIALS/" tmp_mrunner_config.yaml > mrunner_config.yaml
 rm tmp_mrunner_config.yaml
 
-
 # Run job on prometheus.
 # You need to set slurm_url in mrunner_config.yaml (change username to yours).
+cd run
 mrunner --config mrunner_config.yaml --context plgrid_cpu run exp_specs/example_specs/2018_09_25__exp_name.py
 
 
