eae1d79276ce7bc3caf249416c3a93ea7b89649c
KC <konrad.czechowski@gmail.com>
2019-02-11 15:21:44 +0100

    Update scratch_dir.

--- setup_mrunner_venv_and_run.sh
+++ setup_mrunner_venv_and_run.sh
@@ -16,9 +16,9 @@ neptune account login
 pip install git+git://github.com/deepsense-ai/mrunner.git@bd1849cf
 
 # Prepare config file with mrunner context. You can also create contexts in global default config in ~/.configs/mrunner/ .
-sed -r "s/^(\s*)(slurm_url: <username>@pro.cyfronet.pl$)/\1slurm_url: $USERNAME@pro.cyfronet.pl/" mrunner_config_template.yaml > tmp_mrunner_config.yaml
-sed -r "s/^(\s*)(user_id: <initials>$)/\1user_id: $INITIALS/" tmp_mrunner_config.yaml > mrunner_config.yaml
-rm tmp_mrunner_config.yaml
+sed "s/<username>/$USERNAME/g" mrunner_config_template.yaml > tmp_mrunner_config.yaml
+sed -i.bak "s/<initials>/$INITIALS/g" tmp_mrunner_config.yaml
+mv tmp_mrunner_config.yaml mrunner_config.yaml
 # For real experiments change cmd_type in mrunner_config.yaml from srun to sbatch. srun is usefull for remote debug runs.
 
 # Run job on prometheus.
